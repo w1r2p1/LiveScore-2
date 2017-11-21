5731b8764f3e112cb331cb59e292daa791b25c65
Ivan Peric <ivanperic@outlook.com>
2017-11-21 15:55:56 +0100

    C-0002: Added Auth0 Authentication

--- LiveScore/Startup.cs
+++ LiveScore/Startup.cs
@@ -8,22 +8,47 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using LiveScore.Utils.Authorization;
+using Microsoft.AspNetCore.Rewrite;
 
 namespace LiveScore
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+               .SetBasePath(env.ContentRootPath)
+               .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            string domain = $"https://{Configuration["Auth0:Domain"]}/";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.Authority = domain;
+                options.Audience = Configuration["Auth0:ApiIdentifier"];
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("read:scores", policy => policy.Requirements.Add(new HasScopeRequirement("read:scores", domain)));
+                options.AddPolicy("create:scores", policy => policy.Requirements.Add(new HasScopeRequirement("create:scores", domain)));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +58,13 @@ namespace LiveScore
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            var options = new RewriteOptions()
+                .AddRedirectToHttps();
+
+            app.UseRewriter(options);
+
+            app.UseAuthentication();
 
             app.UseMvc();
         }
---
+++ LiveScore/Utils/Authorization/HasScopeRequirements.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveScore.Utils.Authorization
+{
+    public class HasScopeRequirement : AuthorizationHandler<HasScopeRequirement>, IAuthorizationRequirement
+    {
+        private readonly string issuer;
+        private readonly string scope;
+
+        public HasScopeRequirement(string scope, string issuer)
+        {
+            this.scope = scope;
+            this.issuer = issuer;
+        }
+
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, HasScopeRequirement requirement)
+        {
+            if (!context.User.HasClaim(c => c.Type == "scope" && c.Issuer == issuer))
+            {
+                return Task.CompletedTask;
+            }
+
+            var scopes = context.User.FindFirst(c => c.Type == "scope" && c.Issuer == issuer).Value.Split(' ');
+
+            if (scopes.Any(s => s == scope))
+                context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+    }
+}
